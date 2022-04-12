127d5233644adf6ddb38b7425c1fd05174afe980
GuisandeGolfer <DGuisande873@gmail.com>
2022-04-12 09:50:46 -0700

    add notes and test creating word docs

--- main.py
+++ main.py
@@ -2,27 +2,39 @@
 
 import markdown2
 from docx import Document
-from docx.shared import Inches
+from docx.shared import Inches 
 
+# open markdown, convert to HTML
+with open('testfile.md', 'r') as f: # open given md file with read priv.
 
-#document.save("test.docx")
+    text = f.read()
 
+    document = Document('Hello there.docx') 
+        # This method is like "create from template" button in word, doesn't change original or create a new one.
+    document.add_paragraph('lorem ipsum dolor sit amet.')
+        ''' 
+            Find a way to replace 'lorem' with text from markdown, and 
+            designating where I want the text to be placed, maybe there is a
+            replace method or something that takes a location in the word doc.
 
-class Dog:
-   def __init__(self, name, age):
-      self.age = age
-      self.name = name
+            or just physically find the location (line number, line column) by hand
+            because the template is always going to be the same
+        '''
+    document.save('new-document.docx')
 
-   def he_is_so_old(self): # I forgot to add the 'self' and i put "theo" instead of self in the method. 
-      print ("OMG " + self.name + " is already " + self.age)
-
-theo = Dog("theo-beo", "10 months")
-theo.he_is_so_old()
-
-# Output: => "OMG theo-beo is already 10 months"
+    f.close()
+# write a new html file and use 'html' as the content
+# with open('testwebsite.html', 'w') as f:
+    # f.write(html)
 
 # --  -- -- -- -- -- -- -- -- -- --
+'''
+    I need to 'open' the md file, and insert certain blocks of text into certain
+    areas inside of a word document
 
+    Food for thought: Can you convert a word document into a html file?
+        yes, I can, so maybe I can translate md -> html -> word-document
+'''
 # Order of the script
 
 # Import libraries ------
---
+++ testfile.md
@@ -0,0 +1,6 @@
+# Hello!
+
+## H2
+
+- this is going to be a test file for editing markdown with python's open() 
+   method
