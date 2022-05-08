ba35f9e294042d8c312cd35563a3b87ad54da783
GuisandeGolfer <DGuisande873@gmail.com>
2022-05-08 14:29:56 -0700

    delete extra files, and change approach to bs4

--- main.py
+++ main.py
@@ -1,38 +1,34 @@
 #!/usr/bin/python3
-from __future__ import print_function
-
-from datetime import date
-from docx import Document
-from docx.shared import Inches 
-from markdown2 import Markdown
-import mammoth
-
-# Change to absolute location of status report template file [x] 
-
-# change these file locations based on what computer I am on. 
-
-# template = "/Users/dguisande/Library/Group Containers/UBF8T346G9.Office/User Contenf.localized/Templates.localized/\~\$-Berkeley-Meeting-Agenda.dotx"
-#template = "templatecopy.docx"
-
-#document = Document(template)
-
-#iteration = 3
-#current_run = iteration + 1
-
-#document.save("new-template-" + str(current_run)  + ".docx") 
-
-# = = = = = = = = = = = = = = = =
-
-#markdowner = Markdown() 
-
-#print(markdowner.convert("*boo!*"))
-
+# Import Libraries
 # --  -- -- -- -- -- -- -- -- -- --
-with open("templatecopy.docx", "rb") as docx_file:
-    result = mammoth.convert_to_html(docx_file)
-    text = result.value
-    with open('template-output.html', 'w') as text_file:
-        text_file.write(text)
+from __future__ import print_function
+import mammoth
+from bs4 import BeautifulSoup as bs
+import re 
+import os
+# --  -- -- -- -- -- -- -- -- -- --
+
+file_input = "templatecopy.docx"
+
+def change_html_to_wordocx(file):
+
+    with open(file, "rb") as docx_file:
+        result = mammoth.convert_to_html(docx_file) 
+        soup = bs(result, features="lxml")
+        # use soup.find on old text
+        # old_text = soup.find("widget name", {id: "id of what you want to edit"})
+        # then use soup.find on new text with "re.compile" (regex) and then "replace_with()"
+        # new_text = old_text.find(text=re.compile('text you want to edit')).replace_with('new text you want to replace with') 
+        '''
+            try to put specific id's on word-docx templates and then create regex for text and loop through two arrays, 
+            1 with new text, 1 with old text, then replace
+            look at pythonic post on Firebase for faster-ways to iterate over the two arrays/list/dicts/whatever
+        '''
+        with open('template-output.html', 'w') as text_file:
+            text_file.write(prettyHTML)
+            text_file.close()
+
+change_html_to_wordocx(file_input)
 
 '''
     I need to 'open' the md file, and insert certain blocks of text into the method defined on line 11, 
@@ -42,10 +38,3 @@ with open("templatecopy.docx", "rb") as docx_file:
     Food for thought: Can you convert a word document into a html file?
         yes, I can, so maybe I can translate md -> html -> word-document
 '''
-
-
-
-
-
-
-
